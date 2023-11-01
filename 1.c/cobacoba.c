b10e3670d6a03cadd8bc46acdf1d6a1b903a8b7b
amandaaulia976 <amandaauliaa976@gmail.com>
2023-11-01 10:01:15 +0700

    tugas trapezoidal.c

---
+++ 1.c/cobacoba.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char nama [50][50];
+    int k;
+    printf("masukkan kalimat: "); gets(nama [k]);
+    strrev(nama [k]);
+    printf("hasil reverse kalimat: "); printf("%s",nama[k]);
+
+    return 0;
+}
